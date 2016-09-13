63fa3b7c1109bbe358e4f752c9731a05c694471b
Viktor Rohlingit config --global user.email viktorrohlin@gmail.com <viktorrohlin@dhcp200.monkeyinferno.com>
2016-09-13 12:55:40 -0700

    in progres changign markup and styling

--- src/js/components/chat-item.jsx
+++ src/js/components/chat-item.jsx
@@ -60,15 +60,15 @@ class WallItem extends React.Component {
     return moment(this.props.item.created_at).format('LT');
   }
 
-  renderQuote() {
-    if (!this.props.item.quote) {
-      return;
-    }
-    if (this.props.type === "quote") {
-      return;
-    }
-    return <WallItem type="quote" db={this.props.db} item={this.props.item.quote} />;
-  }
+  // renderQuote() {
+  //   if (!this.props.item.quote) {
+  //     return;
+  //   }
+  //   if (this.props.type === "quote") {
+  //     return;
+  //   }
+  //   return <WallItem type="quote" db={this.props.db} item={this.props.item.quote} />;
+  // }
 
   interceptHref(e) {
     if (e.target.href) {
@@ -107,24 +107,24 @@ class WallItem extends React.Component {
     this.props.navigate("photo-viewer", {"mediaUrl": e.currentTarget.dataset.mediaUrl});
   }
 
-  renderMedia() {
-    if(!this.props.item.media) {
-      return;
-    }
-    for (var i = 0 ; i< this.props.item.media.length; i++) {
-      this.props.item.media[i].key = i+1;
-    }
-    return (
-      <div className="chat-item--inner--images">
-        {this.props.item.media.map((i) =>
-          <div key={i.key} className={"image"}
-            style={{backgroundImage: "url(" + (i.url) + ")"}}
-            data-media-url={i.url}
-            onClick={this.viewPhoto}>
-          </div>)}
-      </div>
-    )
-  };
+  // renderMedia() {
+  //   if(!this.props.item.media) {
+  //     return;
+  //   }
+  //   for (var i = 0 ; i< this.props.item.media.length; i++) {
+  //     this.props.item.media[i].key = i+1;
+  //   }
+  //   return (
+  //     <div className="chat-item--inner--images">
+  //       {this.props.item.media.map((i) =>
+  //         <div key={i.key} className={"image"}
+  //           style={{backgroundImage: "url(" + (i.url) + ")"}}
+  //           data-media-url={i.url}
+  //           onClick={this.viewPhoto}>
+  //         </div>)}
+  //     </div>
+  //   )
+  // };
 
   reply(e) {
     console.log("reply clicked");
@@ -135,30 +135,119 @@ class WallItem extends React.Component {
     return <div>{this.props.item.viewed_cnt}</div>;
   }
 
-  render() {
-    return (
-      <div className="chat-item chat-item--inner">
-        <div className="chat-item--inner--left">
-          <div className="chat-item--inner--avatar">
-            {this.renderAvatar()}
-          </div>
+  // render() {
+  //   return (
+  //     <div className="chat-item chat-item--inner">
+  //       <div className="chat-item--inner--left">
+  //         <div className="chat-item--inner--avatar">
+  //           {this.renderAvatar()}
+  //         </div>
+  //       </div>
+  //       <div className="chat-item--inner--right">
+  //         <div className="chat-item--inner--meta">
+  //           <span className="chat-item--inner--meta--username">{this.props.item.username}</span>
+  //           <span className="chat-item--inner--meta--time">
+  //             {this.renderTimestamp()}
+  //           </span>
+  //           <div className="chat-item--reply--button" data-post-id={this.props.item.id} onClick={this.reply}></div>
+  //         </div>
+  //         {this.renderContent()}
+  //         {this.renderMedia()}
+  //         {this.renderQuote()}
+  //         {this.renderViewed()}
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
+
+  renderMeta(){
+    if (this.props.type !== 'quote') {
+      return <div className="wall-item--meta">
+        <div className='wall-item--meta--image'>
+          <img role='presentation' src={'test'}/>
         </div>
-        <div className="chat-item--inner--right">
-          <div className="chat-item--inner--meta">
-            <span className="chat-item--inner--meta--username">{this.props.item.username}</span>
-            <span className="chat-item--inner--meta--time">
-              {this.renderTimestamp()}
-            </span>
-            <div className="chat-item--reply--button" data-post-id={this.props.item.id} onClick={this.reply}></div>
-          </div>
-          {this.renderContent()}
-          {this.renderMedia()}
-          {this.renderQuote()}
-          {this.renderViewed()}
+        <div className='wall-item--meta--text'>
+          <span className='wall-item--meta--text--label'>Viktor </span>
+          <span className='wall-item--meta--text--label'> 8 min</span>
+        </div>
+        <div className='wall-item--meta--actions'>
+          <button onClick={this.reply} className='wall-item-action'>
+            <svg width="22px" height="17px" viewBox="0 0 22 17" version="1.1">
+                <path d="M21.7641471,13.76762 C20.0934412,10.06808 16.6811765,5.15236 10.2309706,4.53968 C9.48038235,4.46182 8.89220588,3.81514 8.86729412,3.02158 C8.866,3.00356 8.866,2.98588 8.866,2.96616 C8.866,2.94814 8.866,2.93182 8.86729412,2.9138 L8.86729412,0.7038 L8.866,0.7038 C8.866,0.31518 8.56285294,0 8.19047059,0 C8.02708824,0 7.87761765,0.0612 7.76114706,0.1615 C7.75338235,0.16762 7.74561765,0.1751 7.73720588,0.18258 L0.556470588,6.86018 C0.555823529,6.86154 0.554529412,6.86154 0.552911765,6.86324 L0.464911765,6.94552 C0.177617647,7.23384 0,7.63742 0,8.08418 C0,8.47552 0.137823529,8.83558 0.365264706,9.112 L0.625705882,9.36292 L7.71294118,16.1942 C7.72297059,16.2044 7.73170588,16.21528 7.74141176,16.22412 C7.86176471,16.33462 8.01867647,16.40058 8.19079412,16.40058 C8.56317647,16.40058 8.86632353,16.08676 8.86632353,15.69848 L8.86761765,15.69848 L8.86761765,12.93428 C8.86632353,12.91626 8.86632353,12.89858 8.86632353,12.88056 C8.86632353,12.86254 8.86632353,12.84486 8.86761765,12.82684 C8.89479412,11.9782 9.56417647,11.2999 10.3859412,11.2999 L10.3859412,11.29684 C14.5290588,11.38354 17.9154412,11.93162 20.6253235,14.50746 L20.6269412,14.50746 C20.6764412,14.5571 20.7278824,14.6047 20.7777059,14.65434 L20.779,14.6557 C20.8870588,14.7305 21.0164706,14.77538 21.1559118,14.77538 C21.5295882,14.77538 21.8311176,14.46156 21.8311176,14.07328 C21.8307941,13.96176 21.8081471,13.86044 21.7641471,13.76762 L21.7641471,13.76762 Z" id="Shape" stroke="none" fill="#D2D2D2" fill-rule="evenodd"></path>
+            </svg>
+          </button>
         </div>
       </div>
-    );
+    } else {
+      return null
+    }
   }
+
+  renderMedia(){
+    if(this.props.item.media && this.props.media.item.length){
+      <div className='wall-item--media'>
+        <ul className='wall-item--media--list'>
+           {this.props.item.media.map((i) =>  <div className='media-item'></div>)}
+        </ul>
+      </div>
+    } else {
+      return null
+    }
+  };
+
+  renderMessage(){
+    var message = this.props.item.message;
+    message = {__html: md.render(message)};
+    return <div className='wall-item--message'>
+      <span className='wall-item--message--text'
+        dangerouslySetInnerHTML={message}>
+      </span>
+    </div>
+  }
+
+  renderBar(){
+    if(this.props.type === 'quote'){
+      return <div className='wall-item--bar'></div>    
+    } else {
+      return null
+    }
+  }
+
+ renderFooter(){
+    if (this.props.type !== 'quote') {
+    return <div className="wall-item--footer">
+       <button className="wall-item--footer--views"> Seen by 16 people</button> 
+    </div>
+    } else {
+      return;
+    }
+  }
+
+  renderQuote(){
+    if (!this.props.item.quote) {
+      return;
+    }
+    if (this.props.type === "quote") {
+      return;
+    }
+    return <WallItem type="quote" db={this.props.db} item={this.props.item.quote} />;
+  } 
+
+  render() {
+    return  <div className="wall-item">
+     {this.renderBar()}
+      <div className='wall-item--inner'>
+        {this.renderMeta()}
+        <div className='wall-item--inner--body'>
+          {this.renderMessage()}
+          {this.renderMedia()}
+          {this.renderQuote()}
+        </div>
+        {this.renderFooter()}
+      </div>
+    </div>
+  } 
 }
 
 WallItem.displayName = 'WallItem';
